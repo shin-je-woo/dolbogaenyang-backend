dbd33e5648d1b728c536f0aba4ccdf5a566328e5
shin.jewoo <shinjw0926@naver.com>
2024-04-03 12:40:22 +0900

    feat: MemberPortfolio 도메인 Entity 작성

---
+++ src/main/java/com/whatpl/member/domain/MemberPortfolio.java
@@ -0,0 +1,33 @@
+package com.whatpl.member.domain;
+
+import com.whatpl.attachment.domain.Attachment;
+import com.whatpl.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Entity
+@Table(name = "member_portfolio")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberPortfolio extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "attachment_id")
+    private Attachment attachment;
+
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public MemberPortfolio(Attachment attachment) {
+        this.attachment = attachment;
+    }
+}
