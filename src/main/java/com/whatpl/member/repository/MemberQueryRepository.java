250ee4d9fef6f27546956439e0fd5f28253dc467
shin.jewoo <shinjw0926@naver.com>
2024-04-03 15:48:03 +0900

    feat: Member의 모든 연관관계를 Fetch Join으로 조회하는 Query

--- src/main/java/com/whatpl/member/repository/MemberQueryRepository.java
+++ src/main/java/com/whatpl/member/repository/MemberQueryRepository.java
@@ -2,6 +2,8 @@ package com.whatpl.member.repository;
 
 import com.whatpl.member.domain.Member;
 import com.whatpl.member.domain.SocialType;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
@@ -9,5 +11,6 @@ public interface MemberQueryRepository {
 
     Optional<Member> findMemberWithAllById(long id);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Member> findMemberWithAllBySocialTypeId(SocialType socialType, String socialId);
 }
