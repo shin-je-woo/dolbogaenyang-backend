448bbf45182dfb081545616aedad7fad058f6d15
shin.jewoo <shinjw0926@naver.com>
2024-05-15 19:12:21 +0900

    fix: TEST환경에서 설정한 데이터소스 사용하도록 수정
    - inmemory db (h2) 사용
    - dialect는 mysql 사용

--- src/test/java/com/whatpl/BaseRepositoryTest.java
+++ src/test/java/com/whatpl/BaseRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.whatpl;
 
 import com.whatpl.global.config.JpaConfig;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
@@ -8,5 +11,9 @@ import org.springframework.test.context.ActiveProfiles;
 @DataJpaTest
 @Import(JpaConfig.class)
 @ActiveProfiles(profiles = "test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // @DataJpaTest에서 제공하는 테스트용 DB 사용 X -> application-test.yml에서 정의한 데이터소스 사용
 public abstract class BaseRepositoryTest {
+
+    @Autowired
+    protected EntityManager em;
 }
