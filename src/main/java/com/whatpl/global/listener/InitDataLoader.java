03fa97fdfdf8202c11a6f2d5733221a3b5f01bfb
shin.jewoo <shinjw0926@naver.com>
2024-07-02 01:32:21 +0900

    refactor: Redisson 분산락 AOP 적용

--- src/main/java/com/whatpl/global/listener/InitDataLoader.java
+++ src/main/java/com/whatpl/global/listener/InitDataLoader.java
@@ -1,24 +1,22 @@
 package com.whatpl.global.listener;
 
+import com.whatpl.global.aop.annotation.DistributedLock;
 import com.whatpl.global.common.domain.enums.Career;
 import com.whatpl.global.common.domain.enums.Job;
 import com.whatpl.global.common.domain.enums.Skill;
 import com.whatpl.global.common.domain.enums.WorkTime;
-import com.whatpl.member.domain.*;
+import com.whatpl.member.domain.Member;
 import com.whatpl.member.domain.enums.SocialType;
 import com.whatpl.member.repository.MemberRepository;
 import jakarta.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
@@ -28,29 +26,13 @@ public class InitDataLoader implements ApplicationListener<ApplicationReadyEvent
 
     private static final AtomicLong memberCount = new AtomicLong(0L);
     private final MemberRepository memberRepository;
-    private final RedissonClient redissonClient;
 
     @Override
+    @DistributedLock(name = "app:init")
     @Transactional
     public void onApplicationEvent(@Nonnull ApplicationReadyEvent event) {
         log.info("===== Application 로딩 후 DATA 초기화 작업을 시작합니다. =====");
-        RLock lock = redissonClient.getLock("lock_application_init");
-        try {
-            boolean isLocked = lock.tryLock(10, TimeUnit.SECONDS);
-            if (!isLocked) {
-                log.info("Application 초기화 Lock 획득 실패");
-                return;
-            }
-
-            setupInitMembers();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new RuntimeException("Application 초기화 쓰레드가 인터럽트 되었습니다.");
-        } finally {
-            if(lock.isHeldByCurrentThread()) {
-                lock.unlock();
-            }
-        }
+        setupInitMembers();
         log.info("===== Application 로딩 후 DATA 초기화 작업을 종료합니다. =====");
     }
 
