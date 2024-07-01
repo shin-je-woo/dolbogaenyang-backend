79788df8ede7be0637ad413adbf5de5b37761a9f
shin.jewoo <shinjw0926@naver.com>
2024-07-02 00:16:13 +0900

    feat: Redisson 분산락 적용

--- src/main/java/com/whatpl/member/repository/MemberQueryRepository.java
+++ src/main/java/com/whatpl/member/repository/MemberQueryRepository.java
@@ -2,8 +2,6 @@ package com.whatpl.member.repository;
 
 import com.whatpl.member.domain.Member;
 import com.whatpl.member.domain.enums.SocialType;
-import jakarta.persistence.LockModeType;
-import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
@@ -11,6 +9,5 @@ public interface MemberQueryRepository {
 
     Optional<Member> findMemberWithAllById(long id);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Member> findMemberWithAllBySocialTypeId(SocialType socialType, String socialId);
 }
