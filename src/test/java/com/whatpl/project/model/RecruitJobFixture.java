5e9d6ed553c60f1e8411540e384175dbf35eaf19
shin.jewoo <shinjw0926@naver.com>
2024-07-11 23:23:44 +0900

    test: 프로젝트 수정 검증

--- src/test/java/com/whatpl/project/model/RecruitJobFixture.java
+++ src/test/java/com/whatpl/project/model/RecruitJobFixture.java
@@ -8,7 +8,14 @@ public class RecruitJobFixture {
     public static RecruitJob create(Job job) {
         return RecruitJob.builder()
                 .job(job)
-                .recruitAmount(10)
+                .recruitAmount(5)
+                .build();
+    }
+
+    public static RecruitJob withRecruitAmount(Job job, int recruitAmount) {
+        return RecruitJob.builder()
+                .job(job)
+                .recruitAmount(recruitAmount)
                 .build();
     }
 }
